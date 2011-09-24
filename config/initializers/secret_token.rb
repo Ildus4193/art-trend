e60aadb2d7d9f9735e74c0582d08c0b4e60130ce
ildus523 <ildus523@mail.ru>
2011-09-24 12:21:46 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtTrend::Application.config.secret_token = '405cd63161b8bccac80f79118e20f3f068d317688e456f473b0e01ac66c55e39f5c157f87e8d7afc469413b394ee59d2d3a2f56b6292e1ccfff441b61e33293a'
