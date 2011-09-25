0b7bc664f9ff8851894445b818ea287adc666b0b
ildus523 <ildus523@mail.ru>
2011-09-25 05:19:14 +0400

    Products show, edit pages is modified

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => "400x500>", :thumb => "226x197>" }
+  has_attached_file :avatar, :styles => { :medium => "400x500>", :thumb => "233x207>" }
   default_scope :order => 'title'
   
   def self.search(query)
